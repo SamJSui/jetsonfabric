aedc026ba2ab8ba316b8671c7bf869bcf771311e
Sam Sui <88460848+SamJSui@users.noreply.github.com>
2026-07-03 00:13:09 -0500

    Add runtime HTTP response header

---
+++ runtime/api/http_response.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+namespace jetsonfabric::runtime {
+
+struct HttpResponse {
+    std::string status;
+    std::string content_type;
+    std::string body;
+
+    std::string serialize() const;
+};
+
+HttpResponse json_response(std::string status, std::string body);
+HttpResponse not_found_response();
+
+} // namespace jetsonfabric::runtime
